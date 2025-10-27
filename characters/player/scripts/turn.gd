a0c877c41dec704b3908db968662d46d499690cf
GiorgosAthanasopoulos <spzdoom@gmail.com>
2025-10-27 19:44:14 +0200

    turn functionality is now child of playaer scene for composition

--- characters/player/scripts/turn.gd
+++ characters/player/scripts/turn.gd
@@ -1,4 +1,4 @@
-extends RigidBody2D
+extends Node2D
 
 @export var air_torque: float = 1_000_000
 @export var air_torque_left_wheel_divider: float = 2
@@ -7,16 +7,18 @@ extends RigidBody2D
 @export var flip_limits_deg: Vector2i = Vector2i(100, 260)
 var _flipped_timer: float = 0
 
+@onready var _player: RigidBody2D = get_parent() as RigidBody2D
+
 func _turn(_torque: float, delta: float) -> void:
 	var has_air_torque: bool = _torque != 0
 	if not has_air_torque:
 		return
 
 	var moving_right: bool = Input.is_action_pressed(&'move_right')
-	apply_torque_impulse(_torque * delta if moving_right else _torque / air_torque_left_wheel_divider * delta)
+	_player.apply_torque_impulse(_torque * delta if moving_right else _torque / air_torque_left_wheel_divider * delta)
 
 func _handle_flipping(delta: float) -> void:
-	var abs_rotation_deg: float = abs(rad_to_deg(rotation))
+	var abs_rotation_deg: float = abs(rad_to_deg(_player.rotation))
 	var has_flipped: bool = abs_rotation_deg > flip_limits_deg.x and abs_rotation_deg < flip_limits_deg.y
 
 	if not has_flipped:
